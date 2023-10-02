e39d9b414e59e197714ab75e4e885a1fd620909f
Felipe Seiberlich <philzaiber@outlook.com>
2023-10-02 12:10:12 +0000

    Add name to contact book, create exception and validate it.

--- run.py
+++ run.py
@@ -11,9 +11,30 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('blueook')
+ALLOWED_NAME_CHARACTERS=['a','b','c','d','e','f','g','h','i','j',
+    'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y',
+    'z','A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+    'O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
 
-contact = SHEET.worksheet('contact')
 
-data = contact.get_all_values()
+def get_contact_data():
+    """
+    Get name, phone, location and email contact from the user.
+    """
+    print('Please enter your name and surname.')
+    print('Data should contain only letters.')
+    print('Example: Philip Grant.\n')
 
-print(data)
+    name_str = input('Enter your name here: \n')
+    print('Validating...')
+    if any(x not in ALLOWED_NAME_CHARACTERS for x in name_str):
+        print("error: invalid character\n")
+        get_contact_data()
+    else:
+        print("no error\n")
+        print(f'The name {name_str} is validated!')
+
+get_contact_data()
+        
+
+
