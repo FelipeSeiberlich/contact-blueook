455960dffb3d6ca9bd81be00f7e00c24b7524ff4
Felipe Seiberlich <philzaiber@outlook.com>
2023-10-04 13:48:38 +0000

    Add style to the system and minify code

--- run.py
+++ run.py
@@ -19,33 +19,31 @@ ALLOWED_NAME_CHARACTERS=['a','b','c','d','e','f','g','h','i','j',
 ALLOWED_PHONE_CHARACTERS=['1','2','3','4','5','6','7','8','9','0',' ']
 
 panel = """                                                  
-   ### #           ##   #                       #   
-    #  # # ###     # #  #  # # ### ### ### # # 
-    #  ### ##      ##   #  # # ##  # # # # ##  
-    #  # # ###     # #  ## ### ### ### ### # # 
-    #              ##                               
+        ### #           ##   #                       #   
+         #  # # ###     # #  #  # # ### ### ### # # 
+         #  ### ##      ##   #  # # ##  # # # # ##  
+         #  # # ###     # #  ## ### ### ### ### # # 
+         #              ##                               
 """
-print(Fore.BLUE)
-print(panel)
-print(Fore.YELLOW)
-print('***Welcome to the Smurfs collectors Contact Book***')
+print(Fore.BLUE, panel)
+print(Fore.YELLOW + '     ***Welcome to the Smurfs collectors Contact Book***')
 print(Fore.WHITE)
 smurfette = """                    
 **//***     
 /////    .         
 (#(    ,,,..         
-((/   ,.,. ..               Hi! I am Smurfette. Nice to meet you!
-/((  ..,,***,,      
+((/   ,.,. ..         Hi! I am Smurfette. Nice to meet you! 
+/((  ..,,***,,          
 ((  /*/%%&@#%/        
-/( /(/  *.& (//,       This is a contact book for Smurfs lovers!
+/( /(/  *.& (//,      This is a contact book for Smurfs lovers!
 .((#&(((##(##%(/,,    
 ((%%%&/%%,%((//**          Please follow the instructons below,
 /((%((#/,..,##/*     
-(#   *,...,/,,##((                      and Blueook yourself...
+(#   *,...,/,,##((                     and Blueook yourself...
 /#  ,*.....,,,    
 /(    #%  #(          
 *((  ##%%(###(.    
-.*(....,/......     
+.*(....,/......  
 """
 print(smurfette)
 
@@ -53,8 +51,7 @@ def get_contact_data():
     """
     Get name, phone, location and email contact from the user.
     """
-    print(Fore.WHITE)
-    print(Style.BRIGHT)
+    print(Fore.WHITE, Style.BRIGHT)
     print('Please enter your name and surname.')
     print('Data must contain only letters.')
     print(Fore.YELLOW)
@@ -65,7 +62,8 @@ def get_contact_data():
     print(Fore.BLUE + 'Validating...\n')
     if any(x not in ALLOWED_NAME_CHARACTERS for x in name_str):
         print(Fore.RED +'Error: invalid character. :(\n')
-        print('Smurfs have only letters in their names.')
+        print('Oooops! I will tell Papa Smurf that you did not follow the instructions.\n')
+        print(Fore.GREEN + 'This time use only letters. :)')
         print(Fore.WHITE)
         get_name_data()
     else:
@@ -75,26 +73,43 @@ def get_contact_data():
 
     print('Please enter your Phone Number.')
     print('Data must contain only numbers.\n')
+    print(Fore.YELLOW)
     print('Example: 00 353 892516666\n')
+    print(Fore.WHITE)
     phone_number_str = input('Enter your phone number here: \n')
+    print(' ')
+    print(Fore.BLUE + 'Validating...\n')
     if any(x not in ALLOWED_PHONE_CHARACTERS for x in phone_number_str):
-        print("\nError: Invalid character. :(\n")
+        print(Fore.RED + '\nError: Invalid character. :(\n')
+        print('Israel just managed to Blueook himself.')
+        print(Fore.GREEN)
+        print('You can do it! This time use only numbers. :)')
+        print(Fore.WHITE)
         get_phone_number()
     else:
         print(' ')
-        print("No error. :)")
+        print(Fore.GREEN + 'No error. :)')
         print(f'The number {phone_number_str} was added successfully!\n')
+        print(Fore.WHITE)
     
-    print('The country I am located is...?')
+    print('In which country are you located?\n')
+    print(Fore.YELLOW)
     print('Example: Ireland.\n')
+    print(Fore.WHITE)
     location_str = input('Enter your location here: \n')
     print(' ')
+    print(Fore.GREEN)
     print(f'The location provided is {location_str}.\n')
+    print(Fore.WHITE)
     print('Please enter your email.\n')
+    print(Fore.YELLOW)
     print('Example: phil123@gmail.com.\n')
+    print(Fore.WHITE)
     email_str = input('Enter your email here: \n')
     print(' ')
+    print(Fore.GREEN)
     print(f'The email provided is {email_str}\n')
+    print(Fore.WHITE)
 
 def get_name_data():
     """
@@ -102,14 +117,20 @@ def get_name_data():
     """
     name_str = input('Enter your name here: \n')
     print(' ')
+    print(Fore.BLUE)
     print('Validating...\n')
     if any(x not in ALLOWED_NAME_CHARACTERS for x in name_str):
-        print("Error: invalid character\n")
+        print(Fore.RED + "Error: invalid character\n")
+        print('Oh no! Gargamel is going to catch you.')
+        print(Fore.GREEN + 'Use only letters to avoid this.')
+        print(Fore.WHITE)
         get_name_data()
     else:
+        print(Fore.GREEN)
         print("No error. :)\n")
         print(f'The name {name_str} was added successfully!')
         print(' ')
+        print(Fore.WHITE)
 
 def get_phone_number():
     """
@@ -122,21 +143,26 @@ def get_phone_number():
         print(' ')
     else:
         print(' ')
-        print("No error. :)")
+        print(Fore.GREEN)
+        print("No error. :)\n")
         print(f'The number {phone_number_str} was added successfully!\n')
+        print(Fore.WHITE)
 
 def update_contact_worksheet(data):
     """
     Update worksheet with data values, add a new row with the list data provided.
     """
+    print(Fore.BLUE)
     print('Updating contact worksheet...')
     contact = SHEET.worksheet('contact')
     contact.append_row(data)
     print('Contact worksheet updated successfully!\n')
+    print(Fore.WHITE)
     add_new_entry()
 
 def add_new_entry():
-    new_entry = input('Would you like to add a new collector to the Blueook y/n?')
+    print(Fore.WHITE)
+    new_entry = input('Would you like to add a new collector to the Blueook? y/n: ')
     if new_entry == 'y' or new_entry == 'Y':
         print(' ')
         get_contact_data()
@@ -144,8 +170,9 @@ def add_new_entry():
         update_contact_worksheet(data)
     else:
         print(' ')
+        print(Fore.BLUE)
         print('Thanks for using The Blueook.')
-        print('See you soon!')
+        print('See you soon!\n')
  
 def main():
     contact_data = (f'{name_str}, {phone_number_str}, {location_str}, {email_str}')
