9897258517dfadcb76710eb01904abf4528c7c61
Felipe Seiberlich <philzaiber@outlook.com>
2023-10-07 17:09:59 +0000

    Create margin in the program

--- run.py
+++ run.py
@@ -32,18 +32,18 @@ print(Fore.WHITE)
 
 def important_message():
     print('                                 IMPORTANT\n')
-    print('* As this is a public dataset we require your consent to share')
-    print('  your contact details with other members of The Blueook Community.\n')
-    print('* You have a choice between sharing your phone number or email. :)\n')
-    print('* Please remember to be polite and respectful to all our members.\n')
-    important = input(Fore.YELLOW + '◔◡◔  I hereby consent to sharing my contact details on The Blueook system. y/n: \n')
+    print(' * As this is a public dataset we require your consent to share')
+    print('   your contact details with other members of The Blueook Community.\n')
+    print(' * You have a choice between sharing your phone number or email. :)\n')
+    print(' * Please remember to be polite and respectful to all our members.\n')
+    important = input(Fore.YELLOW + ' ◔  I hereby consent to sharing my contact details on The Blueook system. y/n: \n')
     print(' ')
     if important == 'y' or important == 'Y':
         print(' ')
 
     elif important == 'n' or important == 'N':
-        print(Fore.WHITE + 'Hi! My name is Azriel and I am a Blueook member.')
-        print('I decided to share only my email and now I am part of this community.')          
+        print(Fore.WHITE + ' Hi! My name is Azriel and I am a Blueook member.')
+        print(' I decided to share only my email and now I am part of this community.')          
         israel = """
 :+**##:                     
    .  :#=                         Bye-bye!
@@ -59,8 +59,8 @@ def important_message():
         print(Fore.RED)
         print(israel)
         print(Fore.WHITE)
-        print('We appreciated your visit.')
-        print('We hope to see you again!\n')
+        print(' * We appreciated your visit.')
+        print(' * We hope to see you again!\n')
         exit_program()
     else:
         panel = """                                                  
@@ -72,9 +72,9 @@ def important_message():
 """
         print(Fore.BLUE, panel)
         print(Fore.WHITE)
-        print('Ooops! Not quite right.\n')
-        print(Fore.GREEN + 'If you want to say YES type "y".')
-        print(Fore.RED + 'If you want to say NO type "n".')
+        print(' * Ooops! Not quite right.\n')
+        print(Fore.GREEN + ' * If you want to say YES type "y".')
+        print(Fore.RED + ' * If you want to say NO type "n".')
         print(Fore.WHITE)
         main()
 
@@ -111,34 +111,34 @@ def get_contact_data():
 """
     print(smurfette)
     print(Fore.WHITE, Style.BRIGHT)
-    print('* Please enter your name and surname.')
-    print('* Data must contain only letters.')
+    print(' * Please enter your name and surname.')
+    print(' * Data must contain only letters.')
     print(Fore.YELLOW)
-    print('> Example: Philip Grant.\n')
+    print(' > Example: Philip Grant.\n')
     print(Fore.CYAN + '--------------------------------------------------------------S2')
     name_str = get_name_data()
     print(Fore.CYAN + '--------------------------------------------------------------S2')    
-    print(Fore.WHITE + '* Please enter your Phone Number.')
-    print('* Data must contain only numbers.\n')
-    print(Fore.YELLOW + '> Example: 00 353 892516666\n')
-    print(Fore.RED + '* If you do not want to share your phone number.')
-    print(Fore.WHITE + '* Enter a single zero "0".\n')
-    print(Fore.YELLOW + '> Example: 0\n')
+    print(Fore.WHITE + ' * Please enter your Phone Number.')
+    print(' * Data must contain only numbers.\n')
+    print(Fore.YELLOW + ' > Example: 00 353 892516666\n')
+    print(Fore.RED + ' * If you do not want to share your phone number.')
+    print(Fore.WHITE + ' * Enter a single zero "0".\n')
+    print(Fore.YELLOW + ' > Example: 0\n')
     print(Fore.CYAN + '--------------------------------------------------------------S2')
     phone_number_str = get_phone_number()
     print(Fore.CYAN + '--------------------------------------------------------------S2')    
-    print(Fore.WHITE + '* In which country are you located?\n')
-    print(Fore.YELLOW + '> Example: Ireland.\n')
-    location_str = input(Fore.WHITE + '◔◡◔  Enter your location here: \n')
+    print(Fore.WHITE + ' * In which country are you located?\n')
+    print(Fore.YELLOW + ' > Example: Ireland.\n')
+    location_str = input(Fore.WHITE + ' ◔ Enter your location here: \n')
     print(Fore.GREEN)
-    print(f'* The location provided is {location_str}.\n')
+    print(f' * The location provided is {location_str}.\n')
     print(Fore.CYAN + '--------------------------------------------------------------S2')
-    print(Fore.WHITE + '* Please enter your email.\n')
-    print(Fore.YELLOW + '> Example: phil123@gmail.com.\n')
-    email_str = input(Fore.WHITE + '◔◡◔  Enter your email here: \n')
+    print(Fore.WHITE + ' * Please enter your email.\n')
+    print(Fore.YELLOW + ' > Example: phil123@gmail.com.\n')
+    email_str = input(Fore.WHITE + ' ◔  Enter your email here: \n')
     print(' ')
     print(Fore.GREEN)
-    print(f'* The email provided is {email_str}\n')
+    print(f' * The email provided is {email_str}\n')
     print(Fore.WHITE)
     return [name_str, phone_number_str, location_str, email_str]
 
@@ -146,20 +146,20 @@ def get_name_data():
     """
     Get Name and Surname data from the user.
     """
-    name_str = input(Fore.WHITE + '◔◡◔  Enter your name here: \n')
+    name_str = input(Fore.WHITE + ' ◔  Enter your name here: \n')
     print(' ')
     print(Fore.BLUE)
-    print('*_* Validating...\n')
+    print(' *_* Validating...\n')
     if any(x not in ALLOWED_NAME_CHARACTERS for x in name_str):
-        print(Fore.RED + "* Error: invalid character\n")
-        print('* Oh no! Gargamel is going to catch you.')
-        print(Fore.GREEN + '* Use only letters to avoid this.')
+        print(Fore.RED + " * Error: invalid character\n")
+        print(' * Oh no! Gargamel is going to catch you.')
+        print(Fore.GREEN + ' * Use only letters to avoid this.')
         print(Fore.WHITE)
         name_str = get_name_data()
     else:
         print(Fore.GREEN)
-        print("* No error. :)\n")
-        print(f'* The name {name_str} was added successfully!')
+        print(" * No error. :)\n")
+        print(f' * The name {name_str} was added successfully!')
         print(' ')
         print(Fore.WHITE)
     return name_str    
@@ -167,18 +167,18 @@ def get_phone_number():
     """
     Get Phone number data from the user.
     """
-    contact_phone_number_str = input(Fore.WHITE + '◔◡◔  Enter your phone number here: \n')
+    contact_phone_number_str = input(Fore.WHITE + ' ◔  Enter your phone number here: \n')
     print(Fore.BLUE)
-    print('*_* Validating...\n')
+    print(' *_* Validating...\n')
     if any(x not in ALLOWED_PHONE_CHARACTERS for x in contact_phone_number_str):
-        print(Fore.RED + "* Error: Invalid character, please enter numbers only.")
+        print(Fore.RED + " * Error: Invalid character, please enter numbers only.")
         print(Fore.WHITE)
         contact_phone_number_str = get_phone_number()
         print(' ')
     else:
         print(Fore.GREEN)
-        print("* No error. :)\n")
-        print(f'* The number {contact_phone_number_str} was added successfully!\n')
+        print(" * No error. :)\n")
+        print(f' * The number {contact_phone_number_str} was added successfully!\n')
     return contact_phone_number_str
 
 def update_contact_worksheet(data):
@@ -201,17 +201,17 @@ def update_contact_worksheet(data):
     ##**++=--::::::.:=*+:     
    ++****+==-------::---.  Mushroom house. 
     """
-    print(Fore.BLUE + '*_* Updating contact worksheet...')
+    print(Fore.BLUE + ' *_* Updating contact worksheet...')
     contact = SHEET.worksheet('contact')
     contact.append_row(data)
-    print('*_* Contact worksheet updated successfully!\n')
+    print(' *_* Contact worksheet updated successfully!\n')
     print(Fore.WHITE)
     print(house)
     print(' ')
 
 def display_data():
     print(Fore.CYAN + '-----------------------------------------------------------------------------------S2')
-    display_contact = input(Fore.WHITE + '◔◡◔  Would you like to display the collectors contact list? y/n: \n')
+    display_contact = input(Fore.WHITE + ' ◔  Would you like to display the collectors contact list? y/n: \n')
     print(' ')
     if display_contact == 'y' or display_contact == 'Y':
         contact = SHEET.worksheet('contact')
@@ -233,14 +233,14 @@ def display_data():
         print(Fore.RED)
         print(israel)
         print(Fore.BLUE)
-        print('* Thanks for using The Blueook.')
-        print('* See you soon!\n')
+        print(' * Thanks for using The Blueook.')
+        print(' * See you soon!\n')
         print(Fore.WHITE)
 
 def exit_blueook():
     print(' ')
     print(Fore.CYAN + '-----------------------------------------------------------------------------------S2')
-    bye_blueook = input(Fore.WHITE + '◔◡◔  Would you like to exit The Blueook? y/n: \n')
+    bye_blueook = input(Fore.WHITE + ' ◔  Would you like to exit The Blueook? y/n: \n')
     print(' ')
     if bye_blueook == 'y' or bye_blueook == 'Y':
         israel = """
@@ -258,13 +258,13 @@ def exit_blueook():
         print(Fore.RED)
         print(israel)
         print(Fore.BLUE)
-        print('* Thanks for using The Blueook.')
-        print('* See you soon!\n')
+        print(' * Thanks for using The Blueook.')
+        print(' * See you soon!\n')
         print(Fore.WHITE)
     else:
-        print(Fore.GREEN + '* If you want to add a new contact then please exit and re-start the program.\n')
-        print(Fore.BLUE + '* If you want to access the contact list one more time type "y" or "Y" below.\n')
-        print(Fore.RED + '* If you want to exit The blueook type "n" or "N" below.\n')
+        print(Fore.GREEN + ' * If you want to add a new contact then please exit and re-start the program.\n')
+        print(Fore.BLUE + ' * If you want to access the contact list one more time type "y" or "Y" below.\n')
+        print(Fore.RED + ' * If you want to exit The blueook type "n" or "N" below.\n')
         display_data()
 
 def exit_program():
